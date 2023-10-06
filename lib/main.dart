290508b1d465052b5b4cce2764d5ada7e7913677
sitharaj88 <sitharaj.info@gmail.com>
2023-10-06 07:11:19 +0530

    notifier issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: MultiProvider(
-          providers: [
-            Provider<TodoViewModel>(
-              create: (_) => TodoViewModel(),
-            ),
-          ],
+        home: ChangeNotifierProvider(
+          create: (context) => TodoViewModel(),
           child: const TodoPage(),
         ));
   }
--- lib/presentation/pages/todo.dart
+++ lib/presentation/pages/todo.dart
@@ -3,33 +3,32 @@ import 'package:flutter_clean_architecture/domain/entities/todo.dart';
 import 'package:flutter_clean_architecture/presentation/viewmodel/todo.dart';
 import 'package:provider/provider.dart';
 
-class TodoPage extends StatefulWidget {
+class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
-  TodoPageState createState() => TodoPageState();
-}
-
-class TodoPageState extends State<TodoPage> {
-  @override
   Widget build(BuildContext context) {
-    final todos = Provider.of<TodoViewModel>(context).todos;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo'),
       ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todos[index].title),
-            trailing: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () {
-                Provider.of<TodoViewModel>(context, listen: false)
-                    .delete(todos[index]);
-              },
-            ),
+      body: Consumer<TodoViewModel>(
+        builder: (context, todoViewModel, child) {
+          return ListView.builder(
+            itemCount: todoViewModel.todos.length,
+            itemBuilder: (context, index) {
+              final todo = todoViewModel.todos[index];
+              return ListTile(
+                title: Text(todo.title),
+                subtitle: Text(todo.description),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    todoViewModel.delete(todo);
+                  },
+                ),
+              );
+            },
           );
         },
       ),
